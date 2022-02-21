599e9cc89e540f64d6752741bff37ce208693ba4
txema <txedav@gmail.com>
2022-02-21 22:19:08 +0100

    working on day 7

--- day_07/src/main.rs
+++ day_07/src/main.rs
@@ -1,3 +1,37 @@
+use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+
+fn part1(filename: &str) -> i32 {
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+    let mut position: Vec<i32> = reader
+        .lines()
+        .next()
+        .unwrap()
+        .unwrap()
+        .split(",")
+        .map(|x| x.parse::<i32>().unwrap())
+        .collect::<Vec<i32>>();
+    position.sort();
+    let total_half = (position.len() as f32 / 2.).floor() as usize;
+    let median = if position.len() % 2 == 0 {
+        ((position[total_half - 1] + position[total_half]) as f32 / 2.) as i32
+    } else {
+        position[total_half]
+    };
+    position.iter().map(|x| (x - median).abs()).sum()
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let filename = env::args().nth(1).expect("Please supply a filename");
+    // Open the file in read-only mode (ignoring errors).
+    
+    let part1_result = part1(&filename);
+    println!("Result of part 1: {}", part1_result);
+
+    // let part2_result = part1(&filename);
+    // println!("Result of part 2: {}", part2_result);
 }
