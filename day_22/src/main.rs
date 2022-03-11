9280c65c5d434e0af18f6136d912c7c5c96db211
txema <txedav@gmail.com>
2022-03-11 13:27:17 +0100

    working on day 22

--- day_22/src/main.rs
+++ day_22/src/main.rs
@@ -1,9 +1,11 @@
+use std::collections::VecDeque;
 use ndarray::{s, Array3};
 use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::collections::HashMap;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Cuboid {
     xlims: (i32, i32),
     ylims: (i32, i32),
@@ -12,6 +14,14 @@ struct Cuboid {
 }
 
 impl Cuboid {
+    fn new() -> Cuboid {
+        Cuboid {
+            xlims: (0, 0),
+            ylims: (0, 0),
+            zlims: (0, 0),
+            sign: 1
+        }
+    }
     fn len(&self) -> u64 {
         let mut res = (self.xlims.1 - self.xlims.0) as i64;
         res *= (self.ylims.1 - self.ylims.0) as i64;
@@ -145,6 +155,16 @@ fn parse_file(filename: &str) -> Vec<Command> {
     for line in reader.lines() {
         let line = line.unwrap();
         let cmd = Command::new(&line);
+        let cub = &cmd.cuboid;
+        if cub.xlims.1 > 51
+            || cub.ylims.1 > 51
+            || cub.zlims.1 > 51
+            || cub.xlims.1 < -50
+            || cub.ylims.1 < -50
+            || cub.zlims.1 < -50
+        {
+            // continue;
+        }
         commands.push(cmd);
     }
     commands
@@ -174,22 +194,98 @@ fn part1(commands: &Vec<Command>) -> u32 {
     matrix.iter().filter(|&x| *x).count() as u32
 }
 
-fn part2(commands: Vec<Command>) -> i64 {
-    let mut operations = CuboidOperation::new();
-    for cmd in commands {
-        if cmd.value {
-            operations.add_on(cmd.cuboid);
-        } else {
-            operations.add_off(cmd.cuboid);
+fn get_ind_set(commands: &Vec<Command>, index: usize) -> Vec<Vec<i32>> {
+    let mut result = Vec::new();
+    if index  == 0 {
+        if commands[0].value {
+            result.push(vec![0]);
         }
+        return result;
     }
-    operations.operate()
+    let prev = get_ind_set(commands, index - 1);
+    println!("{}", index);
+    result.append(&mut prev.clone());
+    if commands[index as usize].value {
+        result.push(vec![index as i32]);
+    }
+    let mut new_clone = prev;
+    for elem in new_clone.iter_mut() {
+        elem.push(-1);
+        elem.push(index as i32);
+    }
+    result.append(&mut new_clone);
+    result
 }
+
+fn make_all_combination_k(N: u32, left: u32, k: u32, result: &mut Vec<Vec<u32>>, tmp: &mut VecDeque<u32>) {
+    if k == 0 {
+        result.push(tmp.clone().into_iter().collect());
+        return;
+    }
+    for i in left..N {
+        tmp.push_back(i);
+        make_all_combination_k(N, i + 1, k - 1, result, tmp);
+        tmp.pop_back();
+    }
+}
+
+fn make_all_combination(N: u32) -> Vec<Vec<u32>> {
+    let mut result = Vec::new();
+    let mut tmp = VecDeque::new();
+    for k in 1..N+1 {
+        make_all_combination_k(N, 0, k, &mut result, &mut tmp);
+    }
+    result
+}
+
+fn get_all_intersections_len(commands: &Vec<Command>) -> HashMap<Vec<u32>, i64> {
+    let mut result = HashMap::new();
+    for combination in make_all_combination(commands.len() as u32) {
+        let mut cuboid = Cuboid::new();
+        let mut not_init = true;
+        for i in &combination {
+            if not_init {
+                cuboid = commands[*i as usize].cuboid.clone();
+                not_init = false;
+            } else {
+                cuboid = cuboid.intersection(&commands[*i as usize].cuboid);
+            }
+        }
+        result.insert(combination, cuboid.len() as i64);
+    }
+    result
+}
+
+fn part2(commands: &Vec<Command>) -> i64 {
+    println!("Creating all operations");
+    let list_operations = get_ind_set(commands, commands.len() - 1);
+    println!("Creating the maps");
+    let dict = get_all_intersections_len(commands);
+    println!("Adding the len");
+    let mut result = 0;
+    for operation in list_operations {
+        let mut new_op = Vec::new();
+        let mut sign = 1 as i64;
+        for ind in operation {
+            if ind == -1 {
+                sign *= -1;
+            } else {
+                new_op.push(ind as u32);
+            }
+        }
+        new_op.sort();
+        result += sign * dict.get(&new_op).unwrap();
+    }
+    result
+}
+
 
 fn main() {
     let filename = env::args().nth(1).expect("Please supply a filename");
     let commands = parse_file(&filename);
+    println!("{}", commands.len());
     println!("Part 1: {}", part1(&commands));
-    println!("Part 2: {}", part2(commands));
+    
+    println!("Part 2: {}", part2(&commands));
     println!("Exp. 2: 2758514936282235");
 }
