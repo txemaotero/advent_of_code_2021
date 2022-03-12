f92eb7a339918c6eeda4498b07e01c533a52f2e7
txema <txedav@gmail.com>
2022-03-12 18:07:06 +0100

    Finished day 22

--- day_22/src/main.rs
+++ day_22/src/main.rs
@@ -1,9 +1,8 @@
-use std::collections::VecDeque;
-use ndarray::{s, Array3};
+use ndarray::{s, Array1, Array3};
+use std::collections::{HashMap, HashSet};
 use std::env;
 use std::fs::File;
-use std::io::{BufRead, BufReader};
-use std::collections::HashMap;
+use std::io::{BufRead, BufReader, Write};
 
 #[derive(Debug, Clone)]
 struct Cuboid {
@@ -13,12 +12,15 @@ struct Cuboid {
 }
 
 impl Cuboid {
-    fn new() -> Cuboid {
-        Cuboid {
-            xlims: (0, 0),
-            ylims: (0, 0),
-            zlims: (0, 0),
-        }
+    fn all_limits(&self) -> Vec<i32> {
+        vec![
+            self.xlims.0,
+            self.xlims.1,
+            self.ylims.0,
+            self.ylims.1,
+            self.zlims.0,
+            self.zlims.1,
+        ]
     }
     fn len(&self) -> u64 {
         let mut res = (self.xlims.1 - self.xlims.0) as i64;
@@ -28,6 +30,12 @@ impl Cuboid {
             panic!("Negative length");
         }
         res as u64
+    }
+    fn overlaps(&self, other: &Self) -> bool {
+        let xoverlap = self.xlims.0 <= other.xlims.1 && other.xlims.0 <= self.xlims.1;
+        let yoverlap = self.ylims.0 <= other.ylims.1 && other.ylims.0 <= self.ylims.1;
+        let zoverlap = self.zlims.0 <= other.zlims.1 && other.zlims.0 <= self.zlims.1;
+        xoverlap && yoverlap && zoverlap
     }
 }
 
@@ -68,16 +76,6 @@ fn parse_file(filename: &str) -> Vec<Command> {
     for line in reader.lines() {
         let line = line.unwrap();
         let cmd = Command::new(&line);
-        let cub = &cmd.cuboid;
-        if cub.xlims.1 > 51
-            || cub.ylims.1 > 51
-            || cub.zlims.1 > 51
-            || cub.xlims.1 < -50
-            || cub.ylims.1 < -50
-            || cub.zlims.1 < -50
-        {
-            // continue;
-        }
         commands.push(cmd);
     }
     commands
@@ -107,39 +105,79 @@ fn part1(commands: &Vec<Command>) -> u32 {
     matrix.iter().filter(|&x| *x).count() as u32
 }
 
-fn make_all_combination_k(N: u32, left: u32, k: u32, result: &mut Vec<Vec<u32>>, tmp: &mut VecDeque<u32>) {
-    if k == 0 {
-        result.push(tmp.clone().into_iter().collect());
-        return;
+fn part2(commands: &Vec<Command>) -> i64 {
+    let mut result = 0;
+    let cmd_len = commands.len();
+    let overlaps = get_overlaps(commands);
+
+    for (cmd_ind, cmd) in commands.iter().enumerate() {
+        print!("\r{} of {}", cmd_ind + 1, cmd_len);
+        std::io::stdout().flush().ok();
+        if cmd.value {
+            result += cmd.cuboid.len() as i64;
+            let inter_cont = intersection_contribution(vec![cmd_ind], commands, &overlaps);
+            result -= inter_cont;
+        }
     }
-    for i in left..N {
-        tmp.push_back(i);
-        make_all_combination_k(N, i + 1, k - 1, result, tmp);
-        tmp.pop_back();
-    }
+    println!("");
+    result
 }
 
-fn make_all_combination(N: u32) -> Vec<Vec<u32>> {
-    let mut result = Vec::new();
-    let mut tmp = VecDeque::new();
-    for k in 1..N+1 {
-        make_all_combination_k(N, 0, k, &mut result, &mut tmp);
+fn intersection_contribution(
+    indexes: Vec<usize>,
+    commands: &Vec<Command>,
+    overlaps: &HashMap<usize, HashSet<usize>>,
+) -> i64 {
+    let last = indexes[indexes.len() - 1];
+    let connections = overlaps.get(&last).unwrap();
+    if connections.is_empty() {
+        return 0;
+    }
+    let mut result = 0;
+    for conn in connections {
+        let mut all_ind = indexes.clone();
+        all_ind.push(*conn);
+        result += get_overlap_len(&all_ind, commands) as i64;
+        result -= intersection_contribution(all_ind, commands, overlaps);
     }
     result
 }
 
-
-fn part2(commands: &Vec<Command>) -> i64 {
-    0
+fn get_overlap_len(indexes: &Vec<usize>, commands: &Vec<Command>) -> u64 {
+    let mut limits = Array1::<i32>::from_vec(commands[indexes[0]].cuboid.all_limits().to_vec());
+    for ind in indexes.iter().skip(1) {
+        let new_lims = &commands[*ind].cuboid.all_limits();
+        for dim in [0, 2, 4] {
+            limits[dim] = limits[dim].max(new_lims[dim]);
+            limits[dim + 1] = limits[dim + 1].min(new_lims[dim + 1]);
+        }
+    }
+    let mut result = 1;
+    for dim in [0, 2, 4] {
+        if limits[dim] > limits[dim + 1] {
+            return 0;
+        }
+        result *= (limits[dim + 1] - limits[dim]) as u64;
+    }
+    result
 }
 
-
+fn get_overlaps(commands: &Vec<Command>) -> HashMap<usize, HashSet<usize>> {
+    let mut result = HashMap::new();
+    for (ind, cmd) in commands.iter().enumerate() {
+        result.insert(ind, HashSet::new());
+        for j in ind + 1..commands.len() {
+            if cmd.cuboid.overlaps(&commands[j].cuboid) {
+                result.get_mut(&ind).unwrap().insert(j);
+            }
+        }
+    }
+    result
+}
 fn main() {
     let filename = env::args().nth(1).expect("Please supply a filename");
     let commands = parse_file(&filename);
-    println!("{}", commands.len());
+
     println!("Part 1: {}", part1(&commands));
-    
     println!("Part 2: {}", part2(&commands));
-    println!("Exp. 2: 2758514936282235");
 }
